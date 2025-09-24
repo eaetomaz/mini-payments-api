74dad757f1765ccb6df34905410baf0de2977d1c
Guilherme Tomaz <guilhermetomaz.bsi@gmail.com>
2025-09-24 20:41:09 -0300

    feat: rotas para consulta de transações, todas ou filtrando

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -1,12 +1,12 @@
 import express from "express";
 import Transaction from "../models/Transaction.js";
-import { where } from "sequelize";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.use(express.Router());
 
-router.get("/", async (req, res) => {
+router.get("/", authMiddleware, async (req, res) => {
     try{
         const transactions = await Transaction.findAll({
             where: { userId: req.user.id },
@@ -16,6 +16,25 @@ router.get("/", async (req, res) => {
         return res.json(transactions);
     } catch (error) {
         return res.status(500).json({ message: "Erro no servidor"});
+    }
+});
+
+router.get("/:id", authMiddleware, async (req, res) => {
+    try{
+        const transaction = await Transaction.findByPk(req.params.id, {
+            include: [
+                { model: Order, attributes: ["id", "status", "totalamount" ] },
+                { model: User, attributes: ["id", "email"] }                
+            ]
+        });
+
+        if(!transaction) return res.status(404).json({ error: "Transação não encontrada" });
+
+        if(req.user.role !== "admin" && transaction.userId !== req.user.id) return res.status(403).json({ error: "Acesso negado" });        
+
+        res.json(transaction);
+    } catch (error) {
+        return res.status(500).json({ error: "Erro ao buscar transação" });
     }
 });
 
